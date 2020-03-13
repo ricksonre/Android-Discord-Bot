9d5facc67594f2751a94b4eb0b416a48dfc3a9ff
MattB70 <mattrb@telus.net>
2020-03-13 10:58:43 -0700

    First attempt at drawer navigation

--- app/src/main/java/com/example/androiddiscordbot/MainActivity.java
+++ app/src/main/java/com/example/androiddiscordbot/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.androiddiscordbot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Home.class);
+                startActivity(i);
+            }
+        });
     }
 }
